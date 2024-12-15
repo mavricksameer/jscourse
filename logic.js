1ad6c852140d983cf1ef7839e17824bf272d4147
mavricksameer <jrsameer108@gmail.com>
2024-12-15 03:25:05 -0800

    js course

---
+++ logic.js
@@ -0,0 +1,92 @@
+let a = 20
+a = 30
+console.log(a);
+
+
+var b = 30 
+b =20 
+console.log(b);
+const z = 11
+console.log(z);
+
+// datatyes
+let f = "sameer" //string
+console.log(f);
+let g = 12312121 //number
+console.log(g);
+
+let u  = null //null
+console.log(u);
+let k  //undefined
+k = "ali"
+console.log(k);
+
+let l = 2
+let o = l
+console.log(o);
+
+
+
+const e =true
+console.log( e)
+let h = ["sameer" , "ali" , 12]
+console.log(h);
+
+let v =  {
+    Name : "sameer" ,
+    age : 19 ,
+    gender : " male"
+}
+console.log(v);
+
+let y 
+console.log(y);
+
+
+let dotaustere = [
+   {
+        Name : "sherox" ,
+        age : 21 ,
+        gender : "male" 
+    } ,
+    {
+        Name : "Aliyan" ,
+        Age : 22
+
+    }
+]
+console.log(dotaustere);
+let nn = {
+   w: [
+    "sameer" , "aliyan" , 21 ,"ali"
+    ] ,
+    r :["dasfa","fdart"]
+    
+}
+console.log(nn);
+
+
+
+let t = "hello"
+let i = "world"
+console.log(`${t}  ${i}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
