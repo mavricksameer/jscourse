ad566e7667c8dd5f9b25e092dbd41ce8ed02214a
mavricksameer <jrsameer108@gmail.com>
2024-12-15 12:00:00 -0800

    pratice

---
+++ course.js
@@ -0,0 +1,72 @@
+//if statement
+
+//  syntax
+// if(condition){
+//     console.log()
+// }else{
+//     console.log()
+// }
+
+
+// let Age = 18
+// if (Age>=18){
+//     console.log("You Are ready to take soso");
+// }else {
+//     console.log("You are not 18");
+    
+// }
+
+
+
+
+// let time = 20
+// if(time>18){
+//     console.log("Alarm is RInging");
+    
+// }else {
+//     console.log("Silent");
+    
+// }
+
+// let player = 14
+// if (player>=18){
+//     console.log("lets play");
+    
+// }else {
+//     console.log("sorry ");
+    
+// }
+
+let Me = 19
+if (Me>=18){
+    console.log("Nikah confirm");
+    
+}else {
+    console.log("thodi dair oe intizr karo");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- logic.js
+++ logic.js
@@ -1,75 +1,258 @@
+// let a = 20
+// a = 30
+// console.log(a);
+
+
+// var b = 30 
+// b =20 
+// console.log(b);
+// const z = 11
+// console.log(z);
+
+// // datatyes
+// let f = "sameer" //string
+// console.log(f);
+// let g = 12312121 //number
+// console.log(g);
+
+// let u  = null //null
+// console.log(u);
+// let k  //undefined
+// k = "ali"
+// console.log(k);
+
+// let l = 2
+// let o = l
+// console.log(o);
+
+
+
+// const e =true
+// console.log( e)
+// let h = ["sameer" , "ali" , 12]
+// console.log(h);
+
+// let v =  {
+//     Name : "sameer" ,
+//     age : 19 ,
+//     gender : " male"
+// }
+// console.log(v);
+
+// let y 
+// console.log(y);
+
+
+// let dotaustere = [
+//    {
+//         Name : "sherox" ,
+//         age : 21 ,
+//         gender : "male" 
+//     } ,
+//     {
+//         Name : "Aliyan" ,
+//         Age : 22
+
+//     }
+// ]
+// console.log(dotaustere);
+// let nn = {
+//    w: [
+//     "sameer" , "aliyan" , 21 ,"ali"
+//     ] ,
+//     r :["dasfa","fdart"]
+    
+// }
+// console.log(nn);
+
+
+
+// let t = "hello"
+// let i = "world"
+// console.log(`${t}  ${i}`)
+
+
+// let courses = ["Javascript" , "CSS" , "HTML" , "React"]
+// console.log(courses .length);
+
+
+
+// // const names = 
+// //   {
+// //     boys :  [ "Aliyan" , "Sameer" , "Faizan"] ,
+// //   Girls : [ "asnas" , "sas" , "xxz"]
+// //  }
+// //  const 
+// //  console.log(filter(names.boys));
+
+// // let list =[
+// //     {Name : "Sameer", Age : "19"}
+// //     {Name : "ali", Age : "22"}
+// // ];
+// let list1 = [1,2,3,4,5,]
+// list1.filter
+// console.log(list1);
+
+
+
+//arthematic operators
+
+// + ,-, *, % ,**
+
+// let a = "12"
+// let b =  12
+// let result = a === b
+// console.log(result)
+
+
+//addition
+// use to add the nuumbers
+// let a = 20
+// let b = 30
+// let c = a + b
+// console.log(c);
+
+//subtraction
+//use to subtrac the numbers
+// let a = 20
+// let b = 30
+// let c = a - b
+// console.log(c);
+
+//multipication
+//use to ultiply the numbers
+// let a = 20 
+// let b = 30
+// let c = a * b
+// console.log(c); 
+
+// division
+// use to divide the numbers
+// let a = 40
+// let b = 20 
+// let c = a / b 
+// console.log(c);
+
+//modulus
+// gives remainder of an integer division
+// let a = 24
+// let b = 10
+// let c = a % b
+// console.log(c);
+
+// exponention
+// let a = 20
+// let b = 3
+// let c = a ** b
+// console.log(c);
+
+// logical operator
+// < >
+// let a = 11
+// let b = 12
+// let c = a < b
+// console.log(c);
+
+//==
+//===
+//=
+//  let a = 20
+//  let b = 20
+//  let c = a === b
+//  console.log(c);
+
+//! 
+// let a = 20 
+// let b = 10
+// let c = a !== b
+// console.log(c);
+
+// let a = 10
+// let b = 10
+// let c =  a != b
+// console.log(c);
+
+// let a = 18
+// if(a!=19){console.log("sorry");}
+    
+// else {console.log("sone");}
+
+   //syntax
+// if(consition){
+//     //line of code
+// }else if{
+//     //line of code
+// }else{
+
+// }
+ 
+
+// let Sameer = 10 ;
+// if (Sameer==20){
+//     console.log("sameer is 20");
+// } 
+// else if (Sameer==22){
+//     console.log("sameer is 19");
+    
+// }
+// else if (Sameer == 99) { 
+//     console.log("sameer is 99");
+// }
+// else if (Sameer==10){
+//     console.log("sameer is 10");
+    
+// } 
+// else if (sameer == 101) { 
+//     console.log("sameer is 101");
+// }
+
+// else {console.log("sameer is nothing");
+// }
+
 let a = 20
-a = 30
-console.log(a);
-
-
-var b = 30 
-b =20 
-console.log(b);
-const z = 11
-console.log(z);
-
-// datatyes
-let f = "sameer" //string
-console.log(f);
-let g = 12312121 //number
-console.log(g);
-
-let u  = null //null
-console.log(u);
-let k  //undefined
-k = "ali"
-console.log(k);
-
-let l = 2
-let o = l
-console.log(o);
-
-
-
-const e =true
-console.log( e)
-let h = ["sameer" , "ali" , 12]
-console.log(h);
-
-let v =  {
-    Name : "sameer" ,
-    age : 19 ,
-    gender : " male"
-}
-console.log(v);
-
-let y 
-console.log(y);
-
-
-let dotaustere = [
-   {
-        Name : "sherox" ,
-        age : 21 ,
-        gender : "male" 
-    } ,
-    {
-        Name : "Aliyan" ,
-        Age : 22
-
-    }
-]
-console.log(dotaustere);
-let nn = {
-   w: [
-    "sameer" , "aliyan" , 21 ,"ali"
-    ] ,
-    r :["dasfa","fdart"]
+if(a>24)
+{
+    console.log("A is 24");
     
 }
-console.log(nn);
+else if (a == 22)
+{
+    console.log("A is 22");
+    
+}
+else if (a>=20){
+    console.log("A is 20");
+    
+}
+
+else {
+    console.log("A does not exist");
+    
+}
+
+ 
 
 
 
-let t = "hello"
-let i = "world"
-console.log(`${t}  ${i}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
 
 
 
