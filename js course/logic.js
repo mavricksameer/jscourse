ec59b7fc2db868e076b5a8f6f4473c7d8fb2fbaa
Sameer Khan <jrsameer108@gmail.com>
2025-01-09 03:46:57 -0800

    Add files via upload

---
+++ js course/logic.js
@@ -0,0 +1,370 @@
+// // let a = 20
+// // a = 30
+// // console.log(a);
+
+// // var b = 30
+// // b =20
+// // console.log(b);
+// // const z = 11
+// // console.log(z);
+
+// // // datatyes
+// // let f = "sameer" //string
+// // console.log(f);
+// // let g = 12312121 //number
+// // console.log(g);
+
+// // let u  = null //null
+// // console.log(u);
+// // let k  //undefined
+// // k = "ali"
+// // console.log(k);
+
+// // let l = 2
+// // let o = l
+// // console.log(o);
+
+// // const e =true
+// // console.log( e)
+// // let h = ["sameer" , "ali" , 12]
+// // console.log(h);
+
+// // let v =  {
+// //     Name : "sameer" ,
+// //     age : 19 ,
+// //     gender : " male"
+// // }
+// // console.log(v);
+
+// // let y
+// // console.log(y);
+
+// // let dotaustere = [
+// //    {
+// //         Name : "sherox" ,
+// //         age : 21 ,
+// //         gender : "male"
+// //     } ,
+// //     {
+// //         Name : "Aliyan" ,
+// //         Age : 22
+
+// //     }
+// // ]
+// // console.log(dotaustere);
+// // let nn = {
+// //    w: [
+// //     "sameer" , "aliyan" , 21 ,"ali"
+// //     ] ,
+// //     r :["dasfa","fdart"]
+
+// // }
+// // console.log(nn);
+
+// // let t = "hello"
+// // let i = "world"
+// // console.log(`${t}  ${i}`)
+
+// // let courses = ["Javascript" , "CSS" , "HTML" , "React"]
+// // console.log(courses .length);
+
+// // // const names =
+// // //   {
+// // //     boys :  [ "Aliyan" , "Sameer" , "Faizan"] ,
+// // //   Girls : [ "asnas" , "sas" , "xxz"]
+// // //  }
+// // //  const
+// // //  console.log(filter(names.boys));
+
+// // // let list =[
+// // //     {Name : "Sameer", Age : "19"}
+// // //     {Name : "ali", Age : "22"}
+// // // ];
+// // let list1 = [1,2,3,4,5,]
+// // list1.filter
+// // console.log(list1);
+
+// //arthematic operators
+
+// // + ,-, *, % ,**
+
+// // let a = "12"
+// // let b =  12
+// // let result = a === b
+// // console.log(result)
+
+// //addition
+// // use to add the nuumbers
+// // let a = 20
+// // let b = 30
+// // let c = a + b
+// // console.log(c);
+
+// //subtraction
+// //use to subtrac the numbers
+// // let a = 20
+// // let b = 30
+// // let c = a - b
+// // console.log(c);
+
+// //multipication
+// //use to ultiply the numbers
+// // let a = 20
+// // let b = 30
+// // let c = a * b
+// // console.log(c);
+
+// // division
+// // use to divide the numbers
+// // let a = 40
+// // let b = 20
+// // let c = a / b
+// // console.log(c);
+
+// modulus
+// gives remainder of an integer division
+// let a = 24
+// let b = 10
+// let c = a % b
+// console.log(c);
+
+// // exponention
+// // let a = 20
+// // let b = 3
+// // let c = a ** b
+// // console.log(c);
+
+// // logical operator
+// // < >
+// // let a = 11
+// // let b = 12
+// // let c = a < b
+// // console.log(c);
+
+// //==
+// //===
+// //=
+// //  let a = 20
+// //  let b = 20
+// //  let c = a === b
+// //  console.log(c);
+
+// //!
+// // let a = 20
+// // let b = 10
+// // let c = a !== b
+// // console.log(c);
+
+// // let a = 10
+// // let b = 10
+// // let c =  a != b
+// // console.log(c);
+
+// // let a = 18
+// // if(a!=19){console.log("sorry");}
+
+// // else {console.log("sone");}
+
+//    //syntax
+// // if(consition){
+// //     //line of code
+// // }else if{
+// //     //line of code
+// // }else{
+
+// // }
+
+// // let Sameer = 10 ;
+// // if (Sameer==20){
+// //     console.log("sameer is 20");
+// // }
+// // else if (Sameer==22){
+// //     console.log("sameer is 19");
+
+// // }
+// // else if (Sameer == 99) {
+// //     console.log("sameer is 99");
+// // }
+// // else if (Sameer==10){
+// //     console.log("sameer is 10");
+
+// // }
+// // else if (sameer == 101) {
+// //     console.log("sameer is 101");
+// // }
+
+// // else {console.log("sameer is nothing");
+// // }
+
+// // let a = 20
+// // if(a>24)
+// // {
+// //     console.log("A is 24");
+
+// // }
+// // else if (a == 22)
+// // {
+// //     console.log("A is 22");
+
+// // }
+// // else if (a>=20){
+// //     console.log("A is 20");
+
+// // }
+// // else {
+// //     console.log("A does not exist");
+
+// // }
+
+// let dotaustere= "19 members"
+// if (dotaustere >= "20 members")
+// {
+//     console.log("Dot austere have 20 members");
+
+// }
+// else if (dotaustere =="19 members")
+// {
+//     console.log("dot austere has 19 members");
+
+// }
+// else if (dotaustere == "21 members")
+//  {
+//     console.log("dot austere has 21 members");
+
+//  }
+//  else {
+//     console.log("dot austere doesnt exists");
+
+//  }
+
+//switch
+// let months = 1;
+// let monthsname;
+
+// switch (months) {
+//   case 1:
+//     monthsname = "january";
+//     break;
+
+// //   case 2:
+// //     monthsname = "february";
+// //     break;
+
+// //     case 3:
+// // }
+// // console.log(monthsname);
+
+// // let fruits = "apple"
+// // switch (fruits)
+// // {
+// //   case "apple":
+// //     console.log("This is an apple");
+
+// //     case 2 :
+// //       "apple"
+
+// // }
+
+// let grade = "p"
+// let result;
+// switch (grade){
+//   case "A":
+//     result = "Grade is excellent"
+//     break;
+//     case "B" :
+//       result = "grade is average"
+//       break;
+// default :
+// result = "result not found"
+
+// }console.log(result);
+
+// let colors = "red"
+// let color;
+// switch (colors)
+// {
+//   case "green" :
+//     color = "green is for nature"
+//     break;
+//     case "red" :
+//       color = "red is for danger"
+//       break ;
+
+// }
+// console.log(color);
+// let sameer = 19
+// if (sameer>=100){
+//   console.log("Sameer is 100 years old");
+// }
+// else if (sameer==19){
+//   console.log("Sameer is 19 years old");
+// }
+// else if (sameer==18){
+//   console.log("Sameer is 18 years old");
+// }
+// else {
+//   console.log("404 error");
+// }
+
+// let a = 10
+// let b = 10
+// let c = a != b
+// console.log(c);
+// switch (expression) {
+//     case value1:
+//         // code block 1;
+//         break;
+//     case value2:
+//          // code block 2;
+//         break;
+//     default:
+//        // default code block;
+// }
+
+// let Class = 2;
+// let students;
+// switch (Class) {
+//   case 1:
+//     students = "There is only one student in class 1";
+//     break;
+//   default:
+//     students = "invalid info";
+//     break;
+//   case 2:
+//     students = "there are only 2 students in class 2";
+//     break;
+// }
+// console.log(students);
+
+// let month = 10;
+// let monthsname;
+// switch (month) {
+//     default :
+//    monthsname = "sorry no data"
+//     break;
+//   case 1:
+//     monthsname = "januray";
+//     break;
+//   case 2:
+//     monthsname = "february";
+//     break;
+//   case 3:
+//     monthsname = "march";
+//     break;
+//   case 4:
+//     monthsname = "aprail";
+//     break;
+// }
+// console.log(monthsname);
+
+// let age = 22
+// if (age < 17) {
+//     console.log("you are child");
+// } else if ( age > 23){
+//     console.log("you r teenager");
+// } else {
+//     console.log("you are adult");
+    
+// }
+
+
